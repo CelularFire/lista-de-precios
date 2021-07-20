c79a7b98f41a7432e30522d764f1e38dcec32fca
CelularFire <86264656+CelularFire@users.noreply.github.com>
2021-07-20 16:13:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-window.location = 'https://drive.google.com/file/d/1bHCIKzWPq1RLMSAZQI6qHdHitnPyiRPJ/view';
+window.location = 'https://drive.google.com/file/d/1VZ_ke8Fr3ZRal2wDAO1W4qLhebyfqwpc/view?usp=sharing';
