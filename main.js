68ef0fa1474a892688d9c5acd670be80031851d8
CelularFire <86264656+CelularFire@users.noreply.github.com>
2021-08-18 15:42:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-window.location = 'https://docs.google.com/spreadsheets/d/1hVsyRProxn7bantO0bcqqT6yurydVKxV/edit?usp=sharing&ouid=114587411475989365729&rtpof=true&sd=true';
+window.location = 'https://docs.google.com/spreadsheets/d/1OcDUd4FJkdkd1R6l265TIqpx7K6VKgkY/edit?usp=sharing&ouid=114587411475989365729&rtpof=true&sd=true';
